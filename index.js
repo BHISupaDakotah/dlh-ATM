ba5fd6e83ae8e4678af17b033b16b477b89d01a7
Dakotah Holmes <holmes.dakotah@gmail.com>
2022-06-23 14:33:45 -0600

    added combined main func with classes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-console.log("hello");
-
-// let userName = window.prompt("What is your name?");
-
-// console.log(userName);
+// Project 2: Virtual ATM Machine (OOP Project)
+// - Build a console app that mimics an atm machine
+// - Persitent balance (crudable [add, remove, null, get])
+// - Main Menu that routes to other menus
+//   *********
+//   Welcome
+//   1) view balance
+//    ..... etc
+//   *********
+// - Redirect back to the main menu
+// - Error handling for withdrawals
+// - Deposit
+// - Exit the program via selection
+// - Receipt
 
 class ATM {
   constructor(userName, accountBalance = 100) {
@@ -12,28 +21,76 @@ class ATM {
 
   deposit(addAmount) {
     this.accountBalance += addAmount;
-    return console.log(this.accountBalance);
+    return window.alert(
+      `you added $ ${addAmount} and your total is $ ${this.accountBalance}`
+    );
   }
 
   withdraw(subAmount) {
     if (subAmount > this.accountBalance) {
-      return "insufficient funds";
+      return window.alert(`sorry, insufficient funds`);
     } else {
       this.accountBalance -= subAmount;
-      return console.log(this.accountBalance);
+      return window.alert(
+        `you removed $ ${subAmount} and your total is $ ${this.accountBalance}`
+      );
     }
   }
 
   currentAmount() {
-    return console.log(
-      `${this.userName} your current balance is ${this.accountBalance}`
+    return window.alert(
+      `${this.userName} your current balance is $ ${this.accountBalance}`
     );
   }
 }
 
-let dakotah = new ATM("dakotah");
+// let dakotah = new ATM("dakotah");
 
-dakotah.currentAmount();
-dakotah.deposit(5);
-dakotah.withdraw(50);
-dakotah.deposit(1);
+// dakotah.currentAmount();
+// dakotah.deposit(5);
+// dakotah.withdraw(50);
+// dakotah.deposit(1);
+// dakotah.withdraw(75);
+
+let useAtm = () => {
+  let userName = window.prompt("What is your name? ");
+
+  let currentUser = new ATM(userName);
+
+  let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  let running = true;
+  while (running) {
+    let userSelection = window.prompt(`
+    welcome!
+    *****************
+    Please select from below...
+    
+    [D] deposit
+    [W] withdrawal
+    [B] view balance
+    [Q] quit
+    `);
+
+    if (userSelection === "D" || userSelection === "d") {
+      addAmount = Number(window.prompt("Enter a deposit amount: "));
+      // if (Number.isInteger(addAmount) === True) {
+      currentUser.deposit(addAmount);
+      // } else {
+      //   window.alert`Please enter a number`;
+      // }
+    } else if (userSelection === "W" || userSelection === "w") {
+      subAmount = Number(window.prompt("Enter a withdrawal amount: "));
+      currentUser.withdraw(subAmount);
+    } else if (userSelection === "B" || userSelection === "b") {
+      currentUser.currentAmount();
+    } else if (userSelection === "Q" || userSelection === "q") {
+      {
+        break;
+      }
+    } else {
+      return window.alert`please select valid input`;
+    }
+  }
+};
+
+useAtm();
